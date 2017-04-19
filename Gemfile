263ba4cb7cb3f03d9e044ad3c73f8736b8fdd7ef
Jan Stevens <jan@playpass.be>
2017-04-19 09:31:33 +0200

    Added coveralls support

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 gemspec
 
 group :test do
+  gem 'coveralls', require: false
   gem 'rdoc'
   gem 'aruba'
   gem 'rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 ::Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each { |f| require_relative f }

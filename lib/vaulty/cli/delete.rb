4097d76cf215a21a6d8c914995f2e979482baa39
Jan Stevens <jan@playpass.be>
2017-04-19 08:50:52 +0200

    Added the delete command

---
+++ lib/vaulty/cli/delete.rb
@@ -0,0 +1,35 @@
+module Vaulty
+  module CLI
+    class Delete < Command
+      attr_reader :catacomb
+      def initialize(catacomb:)
+        @catacomb = catacomb
+      end
+
+      def call
+        banner("Current value #{catacomb.path.inspect}")
+        # Render the tree first so the user knows what we will delete
+        Vaulty::CLI::Tree.call(catacomb)
+        # Confirmation
+        return if prompt.no?('All above data will be recursively deleted! Are you sure?', color: :red)
+
+        banner(catacomb.path, color: :red)
+        # Start deleting
+        delete_recursively(vaulty_tree.children, [catacomb.path])
+        # Delete the route of the path
+        catacomb.delete
+        prompt.ok("Successfully deleted everything in path #{catacomb.path}")
+      end
+
+      private
+
+      def delete_recursively(tree, path = [])
+        Array(tree).each do |folder|
+          current_folder = path + [folder.name]
+          Catacomb.delete(current_folder.join('/')) unless folder.data.empty?
+          delete_recursively(folder.children, current_folder) unless folder.children.empty?
+        end
+      end
+    end
+  end
+end

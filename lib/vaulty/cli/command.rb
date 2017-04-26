a37d5f0a5e2aa769c6410cc16b890fe6cac6ae6f
Jan Stevens <jan@playpass.be>
2017-04-26 21:31:21 +0200

    Added spec and method documentation

--- lib/vaulty/cli/command.rb
+++ lib/vaulty/cli/command.rb
@@ -1,20 +1,46 @@
 module Vaulty
   module CLI
     class Command
+      # Interface method should be defined by subclasses
+      # @raise [NotImplementedError]
+      #
+      def call
+        fail NotImplementedError, "#{inspect}.call is not implemented"
+      end
+
+      # Render a banner
+      # @see Vaulty::Output::Banner
+      # @param [String] msg to be placed in the banner
+      # @param [Symbol] color optional color
+      #
       def banner(msg, color: :green)
         Vaulty::Output::Banner.render(msg, color: color)
       end
 
+      # Renders a table
+      # @param [Array<Array>] data row/columns
+      # @param [Hash] highlgiht options passed to (see Vaulty::Output::HighlightFilter)
+      # @option highlight [Array] :matching strings to highlight
+      # @option highlight [Symbol] :color to be used for highlighting
+      #
       def table(data, highlight: {})
         Vaulty::Output::Table.render(data, highlight: highlight)
       end
 
-      def prompt
-        @prompt ||= TTY::Prompt.new
-      end
-
+      # Initializes a new command with the args and calls it
+      # @param [Hash] args anything
+      #
       def self.call(*args)
         new(*args).call
+      end
+
+      private
+
+      # Returns an instance of {TTY::Prompt}
+      # @return [TTY::Prompt] instance
+      #
+      def prompt
+        @prompt ||= TTY::Prompt.new
       end
     end
   end
---
+++ spec/cli/command_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe Vaulty::CLI::Command do
+  let(:instance) { described_class.new }
+
+  describe '#call' do
+    subject { instance.call }
+    it 'raises an error' do
+      expect { subject }.to raise_exception(NotImplementedError, /.call is not implemented/)
+    end
+  end
+
+  describe '#banner' do
+    subject { instance.banner('banner', color: :red) }
+    it 'delegates to Banner#render' do
+      expect(Vaulty::Output::Banner).to receive(:render).with('banner', color: :red)
+      subject
+    end
+  end
+
+  describe '#table' do
+    subject { instance.table('table', highlight: { matching: ['data'], color: :red }) }
+    it 'delegates to Banner#render' do
+      expect(Vaulty::Output::Table).to receive(:render).with('table', highlight: { matching: ['data'], color: :red })
+      subject
+    end
+  end
+
+  describe '.call' do
+    subject { described_class.call }
+    it 'creates a new instance and calls call' do
+      instance = instance_double(described_class, call: true)
+      expect(described_class).to receive(:new).and_return(instance)
+      expect(instance).to receive(:call)
+      subject
+    end
+  end
+end

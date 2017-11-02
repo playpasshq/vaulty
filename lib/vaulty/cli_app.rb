d5c091a3069aba88ee1f3d6e637be845f131e775
Jan Stevens <jan@playpass.be>
2017-11-02 13:50:13 +0100

    Correctly generate the rdoc command file

--- lib/vaulty/cli_app.rb
+++ lib/vaulty/cli_app.rb
@@ -2,7 +2,7 @@ module Vaulty
   class CLIApp
     extend GLI::App
 
-    program_desc 'Describe your application here'
+    program_desc 'Vault CLI on steriods'
 
     version Vaulty::VERSION
 
@@ -12,14 +12,6 @@ module Vaulty
     accept(Array) do |value|
       Hash[*value.split(':')]
     end
-
-    desc 'Describe some switch here'
-    switch %i(s switch)
-
-    desc 'Describe some flag here'
-    default_value 'the default'
-    arg_name 'The name of the argument'
-    flag %i(f flagname)
 
     desc 'Add a new key/value to the given `PATH`, multiple `key:value` can be provided'
     arg(:path)
@@ -53,70 +45,6 @@ module Vaulty
       c.action do |_global_options, options, _args|
         catacomb = options[:catacomb]
         Vaulty::CLI::Tree.call(catacomb: catacomb)
-      end
-    end
-
-    desc 'Describe backup here'
-    arg_name 'Describe arguments to backup here'
-    command :backup do |c|
-      c.action do |_global_options, _options, _args|
-        puts 'backup command ran'
-      end
-    end
-
-    desc 'Describe convert here'
-    arg_name 'Describe arguments to convert here'
-    command :convert do |c|
-      c.action do |_global_options, _options, _args|
-        puts 'convert command ran'
-      end
-    end
-
-    desc 'Describe diff here'
-    arg_name 'Describe arguments to diff here'
-    command :diff do |c|
-      c.action do |_global_options, _options, _args|
-        puts 'diff command ran'
-      end
-    end
-
-    desc 'Describe generate here'
-    arg_name 'Describe arguments to generate here'
-    command :generate do |c|
-      c.action do |_global_options, _options, _args|
-        puts 'generate command ran'
-      end
-    end
-
-    desc 'Describe provision here'
-    arg_name 'Describe arguments to provision here'
-    command :provision do |c|
-      c.action do |_global_options, _options, _args|
-        puts 'provision command ran'
-      end
-    end
-
-    desc 'Describe remove here'
-    arg_name 'Describe arguments to remove here'
-    command :remove do |c|
-      c.action do |_global_options, _options, _args|
-        puts 'remove command ran'
-      end
-    end
-
-    desc 'Describe restore here'
-    arg_name 'Describe arguments to restore here'
-    command :restore do |c|
-      c.action do |_global_options, _options, _args|
-        puts 'restore command ran'
-      end
-    end
-
-    desc 'Describe write here'
-    arg_name 'Describe arguments to write here'
-    command :write do |c|
-      c.action do |_global_options, _options, _args|
-        puts 'write command ran'
       end
     end
 
